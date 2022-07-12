0a1bff03ee13f37590c730f407c0663a5f9d565a
BT <bt@metaapplebank.com>
2022-07-12 13:28:01 +0100

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,57 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#db6265",
+  primary: "#751113",
+  primaryBright: "#53DEE9",
+  primaryDark: "#0098A1",
+  secondary: "#751113",
+  success: "#751113",
+  warning: "#FFB237",
+};
+
+export const brandColors = {
+  binance: "#F0B90B",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#FAF9FA",
+  backgroundDisabled: "#E9EAEB",
+  contrast: "#191326",
+  invertedContrast: "#FFFFFF",
+  input: "#eeeaf4",
+  inputSecondary: "#d7caec",
+  tertiary: "#EFF4F5",
+  text: "#751113",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#751113",
+  borderColor: "#E9EAEB",
+  card: "#FFFFFF",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+  },
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  secondary: "#9A6AFF",
+  background: "#100C18",
+  backgroundDisabled: "#3c3742",
+  contrast: "#FFFFFF",
+  invertedContrast: "#191326",
+  input: "#483f5a",
+  inputSecondary: "#66578D",
+  primaryDark: "#0098A1",
+  tertiary: "#353547",
+  text: "#EAE2FC",
+  textDisabled: "#666171",
+  textSubtle: "#A28BD4",
+  borderColor: "#524B63",
+  card: "#27262c",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+  },
+};
---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,76 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.applemetabank.com/finance",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.applemetabank.com/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/shekelscoin",
+      },
+      {
+        label: "Docs",
+        href: "",
+      },
+      {
+        label: "Blog",
+        href: "",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "Annoucements",
+        href: "https://t.me/shekelscoin",
+      },
+      {
+        label: "English",
+        href: "https://t.me/shekelscoin",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/shekelscoin",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
